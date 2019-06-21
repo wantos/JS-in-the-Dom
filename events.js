4881b345ed3a6a69839a099c96345d0046b9d52f
Wantono Emmanuel <51445540+wantos@users.noreply.github.com>
2019-06-21 15:55:11 +0300

    Add files via upload

---
+++ events.js
@@ -0,0 +1,9 @@
+// Function to modify the text content of the paragraph
+const changeText = () => {
+    const p = document.querySelector('p');
+
+    p.textContent = "I changed because of an inline event handler.";
+}
+
+
+
