ca0280a0f1353f6dc3f6a41a06a8957b7efd35d4
Wantono Emmanuel <51445540+wantos@users.noreply.github.com>
2019-06-21 15:55:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var element = document.createElement(tagName[, options]);
+
+   	    var element = document.createElement(tagName[, options]);
+        var text = document.createTextNode(data);
+
+  // To-do list ul element
+const todoList = document.querySelector('ul');
+
+// Create new to-do
+const newTodo = document.createElement('li');
+newTodo.textContent = 'Do homework';
